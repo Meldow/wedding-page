699c699d0c54238153494459786d8de97e0cc797
Alexandre Costa <alexandre.costa@farfetch.com>
2019-01-19 19:29:58 +0000

    feat(icon): Add linked icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,47 +1,87 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import { Grid, Image } from 'semantic-ui-react'
+import { Grid, Icon } from 'semantic-ui-react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
+const handPointer = {
+  cursor: 'pointer',
+}
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={ [`gatsby`, `application`, `react`] } />
-
-
-
-    <Grid centered columns={2}>
+    <Grid centered columns={ 1 }>
       <Grid.Column>
-        <Image
-          fluid
-          src='/images/us.png' />
+        <div className="ui fluid image">
+          <img src='/images/us.png' />
+          <div style={ {
+            position: 'absolute',
+            bottom: '50%',
+            left: '50%',
+            width: '100%',
+            height: 'auto',
+            color: 'white',
+          } }>TEST
+          </div>
+        </div>
       </Grid.Column>
-
-      <Grid.Row centered columns={4}>
+      <Grid.Row centered columns={ 2 }>
         <Grid.Column>
-          <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+          <div className="ui fluid image">
+            <img src='/images/save_the_date.png' />
+          </div>
         </Grid.Column>
         <Grid.Column>
-          <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-        </Grid.Column>
-        <Grid.Column>
-          <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-        </Grid.Column>
-      </Grid.Row>
-
-      <Grid.Row centered columns={4}>
-        <Grid.Column>
-          <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-        </Grid.Column>
-        <Grid.Column>
-          <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-        </Grid.Column>
-        <Grid.Column>
-          <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-        </Grid.Column>
-        <Grid.Column>
-          <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+          <Grid.Row centered columns={ 1 }>
+            <Grid.Column>
+              <div>
+                <div style={ handPointer }
+                     onClick={ () => {
+                       window.open('https://goo.gl/maps/ay3SvnUceoK2')
+                     } }
+                >
+                  <Icon
+                    name='instagram'
+                    size='large'
+                    link
+                  />
+                  <span>Share our journey!</span>
+                  <span>#LopesCosta</span>
+                  <span>Tag your photos!</span>
+                </div>
+              </div>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row centered columns={ 1 }>
+            <Grid.Column>
+              <div style={ handPointer }
+                   onClick={ () => {
+                     window.open('https://goo.gl/maps/ay3SvnUceoK2')
+                   } }
+              >
+                <Icon
+                  name='location arrow'
+                  size='large'
+                  link
+                />
+                <span>Marriage ceremony</span>
+              </div>
+              <div style={ handPointer }
+                   onClick={ () => {
+                     window.open('https://goo.gl/maps/ay3SvnUceoK2')
+                   } }
+              >
+                <Icon
+                  name='location arrow'
+                  size='large'
+                  link
+                />
+                <span>Wedding reception</span>
+              </div>
+            </Grid.Column>
+          </Grid.Row>
         </Grid.Column>
       </Grid.Row>
     </Grid>
